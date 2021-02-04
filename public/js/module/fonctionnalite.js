e5deda16634c9428ac455d6cf958d2a499666425
Achraf <hardy1348@live.fr>
2021-02-04 11:43:50 +0100

    fin

--- public/js/module/fonctionnalite.js
+++ public/js/module/fonctionnalite.js
@@ -23,4 +23,80 @@ let myAFaire = document.querySelector('#filtre button')
 let myTerminer = document.querySelectorAll('#filtre button')[1]
 let myTout = document.querySelectorAll('#filtre button')[2]
 
+myInputCreation.addEventListener('keypress', (e) => {
+    if (e.keyCode === 13) {
+        if (myInputCreation.value == "") {
+            alert('Remplissez le champs !');
 
+        } else {
+            nouvelleTache = document.createElement('div')
+            nouvelleTache.className = 'tache'
+
+            nouvelleTache.textContent = myInputCreation.value
+
+            myDivAvecLesTaches.appendChild(nouvelleTache)
+            myDivAvecLesTaches.style = 'padding-bottom: 10px;padding-top: 10px;'
+
+            creationDiv(stock, nouvelleTache, iconEdit, iconCheck, iconSuppr)
+
+
+        }
+    }
+})
+
+myAjout.addEventListener('click', () => {
+    if (myInputCreation.value == "") {
+        alert('Remplissez le champs !');
+
+    } else {
+        nouvelleTache = document.createElement('div')
+        nouvelleTache.className = 'tache'
+
+        nouvelleTache.textContent = myInputCreation.value
+
+        myDivAvecLesTaches.appendChild(nouvelleTache)
+        myDivAvecLesTaches.style = 'padding-bottom: 10px;padding-top: 10px;'
+
+        creationDiv(stock, nouvelleTache, iconEdit, iconCheck, iconSuppr)
+
+
+    }
+
+})
+
+
+let tab;
+
+myAFaire.addEventListener('click', () => {
+    tab = [].slice.call(myDivAvecLesTaches.children)
+    tab.forEach(element => {
+
+        if (element.classList == 'tache fini') {
+            element.classList.add("disparait")
+
+        } else {
+            element.classList.remove("disparait");
+        }
+
+    });
+})
+
+myTerminer.addEventListener('click', () => {
+    tab = [].slice.call(myDivAvecLesTaches.children)
+    tab.forEach(element => {
+        if (element.classList == 'tache pasFini') {
+            element.classList.add("disparait")
+        } else {
+            element.classList.remove("disparait");
+        }
+    });
+})
+
+myTout.addEventListener('click', () => {
+    tab = [].slice.call(myDivAvecLesTaches.children)
+    tab.forEach(element => {
+        if (element.classList == 'tache fini disparait' || element.classList == 'tache pasFini disparait') {
+            element.classList.remove("disparait");
+        }
+    });
+})
